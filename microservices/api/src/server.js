df7e758f7c110f7f9b9dfd46b46fd565b5ecb75d
George_Martin <grgmrtn2312@gmail.com>
2018-02-04 12:40:06 +0530

    anon8

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -38,6 +38,37 @@ app.get('/check_req', function( req, res ) {
   if ( req.query.file_op === 'read' ) {   
     const file_id = req.query.file_id;
     const user_id = req.headers['x-hasura-user-id'];
+    
+    var url= "https://data.also52.hasura-app.io/v1/query"
+    const options = {
+      
+      method: 'POST',
+      body: JSON.stringify(queryObj),
+      headers: {
+        'Content-Type': 'application/json',
+        'x-hasura-user-id': '1',
+        'x-hasura-role': 'admin'
+      }
+    }
+
+     fetchAction(url, options)
+    .then( function( sol ) {
+      return response.json();
+    })
+    .then( function( resp ) {
+      
+      if ( (resp[0].userid).toString() === (user_id).toString() ) {
+        res.status(200).send('ok');
+        return;
+      }
+      res.status(403).send('notok');
+      return;
+    })
+    .catch( function ( resp ) {
+      res.status(403).send('notok');
+      return;
+    });
+  }  else {
     res.status(200).send('ok');
   }
 });
