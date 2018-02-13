9e081bb1517d06f2d415cf9df455b30893c269ad
George_Martin <grgmrtn2312@gmail.com>
2018-02-13 21:36:41 +0530

    file-check api cutdown

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -34,59 +34,7 @@ app.get('/',function(req,res){
   res.send("heyyy");
 })
 
-app.get('/check_req', function( req, res ) {
-  if ( req.query.file_op === 'read' ) {   
-    const file_id = req.query.file_id;
-    const user_id = req.headers['x-hasura-user-id'];
-    
-    var url= "https://data.also52.hasura-app.io/v1/query"
 
-    const queryObj = {
-      "type": "select",
-      "args": {
-          "table": "files",
-          
-          "columns": [
-              "userid"
-          ],
-          "where": {
-              "fileid": {
-                  "$eq": file_id
-              }
-          }
-      }
-    };
-    const options = {
-      method: 'POST',
-      body: JSON.stringify(queryObj),
-      headers: {
-        'Content-Type': 'application/json',
-        'x-hasura-user-id': '1',
-        'x-hasura-role': 'admin'
-      }
-    }
-
-     fetchAction(url, options)
-    .then( function( sol ) {
-      return sol.json();
-    })
-    .then( function( resp ) {
-      
-      if ( (resp[0].userid).toString() === (user_id).toString() ) {
-        res.status(200).send('ok');
-        return;
-      }
-      res.status(403).send('notok');
-      return;
-    })
-    .catch( function ( resp ) {
-      res.status(403).send('notok');
-      return;
-    });
-  }  else {
-    res.status(200).send('ok');
-  }
-});
 /*
 app.get('/check', function( req, res ) {
   
