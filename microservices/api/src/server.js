a4c6e9ad29764d912698a19d60c8373299988474
George_Martin <grgmrtn2312@gmail.com>
2018-02-20 19:55:21 +0530

    customs

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -16,82 +16,13 @@ var bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 
 
-/*app.use(fileUpload());
-
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});*/
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
 
-app.get('/',function(req,res){
-  res.send("heyyy");
-})
 
 
-/*
-app.get('/check_req', function( req, res ) {
-  res.status(200).send('ok');
-  return;
-  if ( req.query.file_op === 'read' ) {   
-
-    const file_id = req.query.file_id;
-    const user_id = req.headers['x-hasura-user-id'];
-    
-    var url= "https://data.also52.hasura-app.io/v1/query";
-    const queryObj = {
-      "type": "select",
-      "args": {
-          "table": "files",
-          "columns": [
-              "userid"
-          ],
-          "where": {
-              "fileid": {
-                  "$eq": file_id
-              }
-          }
-      }
-    };
-    const options = {
-      
-      method: 'POST',
-      body: JSON.stringify(queryObj),
-      headers: {
-        'Content-Type': 'application/json',
-        'x-hasura-user-id': '1',
-        'x-hasura-role': 'admin'
-      }
-    }
-
-     fetchAction(url, options)
-    .then( function( sol ) {
-      return sol.json();
-    })
-    .then( function( resp ) {
-      
-      if ( (resp[0].userid).toString() === (user_id).toString() ) {
-        res.status(200).send('ok');
-        return;
-      }
-      res.status(403).send('notok');
-      return;
-    })
-    .catch( function ( resp ) {
-      res.status(403).send('notok');
-      return;
-    });
-  }  else {
-    res.status(200).send('ok');
-  }
-});
-*/
 
 app.get('/logout',function(req,res){ 
   var auth_token = req.body.data.auth_token;
@@ -300,12 +231,7 @@ app.post('/upload',function(req,res){
   
 
 })
-// Uncomment to add a new route which returns hello world as a JSON
-// app.get('/json', function(req, res) {
-//   res.json({
-//     message: 'Hello world'
-//   });
-// });
+
 
 /*app.post('/download',function(req,res){
   
@@ -336,6 +262,63 @@ app.post('/upload',function(req,res){
   
 });*/
 
+/*
+app.get('/check_req', function( req, res ) {
+  res.status(200).send('ok');
+  return;
+  if ( req.query.file_op === 'read' ) {   
+
+    const file_id = req.query.file_id;
+    const user_id = req.headers['x-hasura-user-id'];
+    
+    var url= "https://data.also52.hasura-app.io/v1/query";
+    const queryObj = {
+      "type": "select",
+      "args": {
+          "table": "files",
+          "columns": [
+              "userid"
+          ],
+          "where": {
+              "fileid": {
+                  "$eq": file_id
+              }
+          }
+      }
+    };
+    const options = {
+      
+      method: 'POST',
+      body: JSON.stringify(queryObj),
+      headers: {
+        'Content-Type': 'application/json',
+        'x-hasura-user-id': '1',
+        'x-hasura-role': 'admin'
+      }
+    }
+
+     fetchAction(url, options)
+    .then( function( sol ) {
+      return sol.json();
+    })
+    .then( function( resp ) {
+      
+      if ( (resp[0].userid).toString() === (user_id).toString() ) {
+        res.status(200).send('ok');
+        return;
+      }
+      res.status(403).send('notok');
+      return;
+    })
+    .catch( function ( resp ) {
+      res.status(403).send('notok');
+      return;
+    });
+  }  else {
+    res.status(200).send('ok');
+  }
+});
+*/
 
 
 // custom 404 page
